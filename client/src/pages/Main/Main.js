8e018820524b1ac1c48496ea0b7d5f7efca74230
[AI6/2반] 박재희 <dalkey23@gmail.com>
2023-06-06 16:44:51 +0900

    fix: 메인페이지 css 수정

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -3,10 +3,9 @@ import { NavLink } from "react-router-dom";
 import axios from "axios";
 
 import BodySlides from "./bodySlides";
-import * as SC from "./MainSC"
+import * as SC from "./MainSC";
 
 const Main = () => {
-
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
@@ -21,28 +20,20 @@ const Main = () => {
     }, []);
 
     return (
-        <div
-            style={{
-                padding: "100px",
-            }}>
-            <SC.Container className="d-flex flex-column min-vh-50">
-                <BodySlides />
-
-                <div
-                    className="d-flex justify-content-around"
-                    style={{
-                        padding: "50px",
-                    }}>
-                    {categories.map((category) => {
-                        return <li key={category.categoryId}>
+        <SC.Container>
+            <BodySlides />
+            <ul>
+                {categories.map((category) => {
+                    return (
+                        <li key={category.categoryId}>
                             <NavLink to={`/categories/${category.categoryId}`}>
                                 {category.name}
                             </NavLink>
-                        </li>;
-                    })}
-                </div>
-            </SC.Container>
-        </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </SC.Container>
     );
 };
 
--- client/src/pages/Main/MainSC.js
+++ client/src/pages/Main/MainSC.js
@@ -1,5 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display : flex;
+    flex-direction : column;
+
+    & ul {
+        padding : 1em 5em;
+        display : flex;
+        justify-content : space-between;
+        list-style : none;
+    
+    }
+
+    & a {
+        text-decoration : none;
+        color : black   
+    }
 
 `
